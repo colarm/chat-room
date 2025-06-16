54c67b7d5702bf620af8d3adbcb4592fff8533fe
Colarm <haichzh@gmail.com>
2025-06-16 23:21:24 +0100

    Optimized structure and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppProvider } from './components/AppContext';
-import NotFound from './pages/NotFound';
-import Main from './pages/Main';
-import Chat from './pages/Chat';
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./components/AppContext";
+import NotFound from "./pages/NotFound/NotFound";
+import Main from "./pages/Main/Main";
+import Chat from "./pages/Chat/Chat";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
         <AppProvider>
           <Routes>
-            <Route path='/' Component={Main} />
-            <Route path='/Chat' Component={Chat} />
-            <Route path='*' Component={NotFound} />
+            <Route path="/" Component={Main} />
+            <Route path="/Chat" Component={Chat} />
+            <Route path="*" Component={NotFound} />
           </Routes>
         </AppProvider>
       </Router>
